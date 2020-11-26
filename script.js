ff8b52698db5feb428c684dc352cfdb8dc052b2a
Elif Balci <mrselifbalci@gmail.com>
2020-11-26 01:27:05 +0000

    All files added.

---
+++ script.js
@@ -0,0 +1,62 @@
+let userName = prompt("What is your name?");
+let userQuestion = prompt("Ask your question.");
+if (userName != "") {
+  console.log(`Hello ${userName}.`);
+} else {
+  console.log(`Hello guest.`);
+}
+console.log(`Your question is: '${userQuestion}'.`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = "";
+
+if (randomNumber === 0) {
+  eightBall = "It is certain.";
+} else if (randomNumber === 1) {
+  eightBall = "It is decidedly so.";
+} else if (randomNumber === 2) {
+  eightBall = "Reply hazy try again.";
+} else if (randomNumber === 3) {
+  eightBall = "Cannot predict now.";
+} else if (randomNumber === 4) {
+  eightBall = "Do not count on it.";
+} else if (randomNumber === 5) {
+  eightBall = "My sources say no.";
+} else if (randomNumber === 6) {
+  eightBall = "Outlook not so good.";
+} else {
+  eightBall = "Signs point to yes.";
+}
+
+//Done the same thing with switch statement below.
+
+// switch (randomNumber) {
+//   case 0:
+//     eightBall = "It is certain.";
+//     break;
+//   case 1:
+//     eightBall = "It is decidedly so.";
+//     break;
+//   case 2:
+//     eightBall = "Repl hazy try again.";
+//     break;
+//   case 3:
+//     eightBall = "Cannot predict now.";
+//     break;
+//   case 4:
+//     eightBall = "Do not count on it.";
+//     break;
+//   case 5:
+//     eightBall = "My sources says no.";
+//     break;
+//   case 6:
+//     eightBall = "Outlook not so good.";
+//     break;
+//   default:
+//     eightBall = "Signs point to yes.";
+//     break;
+// }
+
+setTimeout(function () {
+  alert(`The eight ball answered: ${eightBall}`);
+}, 2000);
